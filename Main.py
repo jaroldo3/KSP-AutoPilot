596b1492ba520b3be2ba4ed69660884c39607bd4
Jarred <jaroldosae@gmail.com>
2020-12-07 08:57:40 -0600

    Add new stuff

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 
 import krpc
 import Launch
-conn = krpc.connect(name='Hello World')
+import OrbitalAscent
+conn = krpc.connect(name='Main')
 vessel = conn.space_center.active_vessel
 print(vessel.name)
 Launch.main(vessel)
+OrbitalAscent.main(vessel, conn)
